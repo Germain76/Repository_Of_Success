8059ffaff56e785f2d5f95ee0f149a7f322ce7b0
GErmain <claude.raynal.germain@gmail.com>
2021-02-18 10:16:50 +0100

    widget imageAndText fini

--- packages/my-gatsby-project/src/cms/editorsWidgets/imageInText.js
+++ packages/my-gatsby-project/src/cms/editorsWidgets/imageInText.js
@@ -3,12 +3,12 @@ const imageInText = {
     label: "Image et texte",
     fields:
         [
-  
-        { name: 'image', label: 'Image', widget: 'image'},
-        {name: 'author', label: 'Author', widget: 'markdown'},
-        {name: 'title', label: 'Title', widget: 'markdown'}, 
-        { name: 'text', label: 'Texte', widget: 'text' },
-    ],
+
+            { name: 'image', label: 'Image', widget: 'image' },
+            { name: 'author', label: 'Author', widget: 'markdown' },
+            { name: 'title', label: 'Title', widget: 'markdown' },
+            { name: 'text', label: 'Texte', widget: 'text' },
+        ],
 
 
 
@@ -17,28 +17,32 @@ const imageInText = {
     fromBlock: function (match) {
         console.log(match)
         let content = match && match[0] && match[0].matchAll(/"(.+?)"/g)
+        let contentText = match && match[0] && match[0].match(/>(.+?)</)
         content = Array.from(content)
-        let firstMatch = content[0]
-        let secondMatch = content[1]
-        let thirdMatch = content[2]
-        let fourMatch = content [3]
-        console.log(firstMatch[1])
-        console.log(secondMatch[1])
-        console.log(thirdMatch[1])
+        let matchImage = content[0]
+        let matchAuthor = content[1]
+        let matchTitle = content[2]
+        console.log(matchImage[1])
+        console.log(matchAuthor[1])
+        console.log(matchTitle[1])
+        console.log(contentText[1])
 
 
+        if (!(content && contentText && content[0] && content[1] && content[2] && contentText[1])) {
+            return { image: "", author: "", title: "", text: "" }
+        }
+        else {
+            return { image:matchImage[1], author:matchAuthor[1], title: matchTitle[1], text: contentText[1]}
+        }
 
-       
-        return {text :"", image :""}
-                  
     },
-    toBlock: function ({image,author,title,text}) {
+    toBlock: function ({ image, author, title, text }) {
         //console.log({image,author,title,text})
         return `<MdxTextImage image="${image}" author="${author}" title="${title}">  ${text} </MdxTextImage>`
     },
-    toPreview: function ({image,author,title,text}) {
+    toPreview: function ({ image, author, title, text }) {
         return `<MdxTextImage image="${image}" author="${author}" title="${title}">  ${text} </MdxTextImage>`
-       
+
     }
 }
 export default imageInText;
