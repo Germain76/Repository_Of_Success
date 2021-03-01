74cf1258c4c8562e9d11c6746b5c4dd575a62333
Sylvain Joffraud <sjoffraud@4lw.fr>
2021-03-01 16:38:34 +0100

    Fix controlled input

--- packages/my-gatsby-project/src/components/formular/email/Email_Data.js
+++ packages/my-gatsby-project/src/components/formular/email/Email_Data.js
@@ -1,16 +1,19 @@
 import React, { useState } from 'react';
 
 
-export const Email_Data = (props) => {
+export const Email_Data = ({text, onChange}) => {
+    const [value, setValue] = useState(text);
     const handleChange = (event) => {
+        setValue(event.target.text);
         // TODO : Validation ici   
-        props.onChange(event.target.text);
+        onChange(event.target.text);
     }
     
     return (
         <label>Email 
     <input 
-    value={props.text} 
+    type="email"
+    value={value} 
     onChange={handleChange} 
     placeholder="Veuillez saisir votre adresse Email dans ce champ s'il vous plait"
     />
